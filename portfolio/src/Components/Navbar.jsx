a6c0e7802e15d1391a085b8b18eac32fb0a8f730
Kiritostack <kakeshiash@gmail.com>
2026-02-05 01:56:07 +0530

    few changes

--- portfolio/src/Components/Navbar.jsx
+++ portfolio/src/Components/Navbar.jsx
@@ -1,12 +1,12 @@
 import './Navbar.css'
-export default function({theme,settheme}){
+export default function Navbar({theme,settheme}){
     return(
         <nav className='Navbar'>
           <h2 className='logo'>Kunal.dev</h2>
           <ul className='nav-links'>
             <li><a href="#Projects">Projects</a></li>
             <li><a href="#Skills">Skills</a></li>
-            <li><a href="Resume.pdf" download>Resume</a></li>
+            <li><a href="Kunal_Kumar_Resume.pdf" download>Resume</a></li>
           </ul>
           <button className='btn' onClick={()=>{settheme(theme==="Dark"?"Light":"Dark")}}>{theme==="Dark"?"Light":"Dark"}</button>
         </nav>
