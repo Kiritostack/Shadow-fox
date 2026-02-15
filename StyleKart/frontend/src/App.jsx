e06fabce66b59da53c7bddf1f511e8a3be23ad83
Kunal Kumar <kakeshiash@gmail.com>
2026-02-15 12:55:02 +0530

    loop

--- StyleKart/frontend/src/App.jsx
+++ StyleKart/frontend/src/App.jsx
@@ -2,20 +2,26 @@ import Banneer from "./Components/Banneer";
 import Navbar from "./Components/Navbar";
 import ProductCard from "./Components/ProductCard";
 import products from "./data/products";
-import "./App.css"
+import "./App.css";
 function app(){
 return (
   <div>
     <Navbar/>
     <Banneer/>
+    <div className="slider">
+ <div className="track">
     <div className="products">
-      {products.map((item)=>(
-      <ProductCard 
-      key={item.id}
-      name={item.name}
-      price={item.price}
-      image={item.image}/>))}
+      {[...products, ...products].map((item, i) => (
+        <ProductCard
+          key={i}
+          name={item.name}
+          price={item.price}
+          image={item.image}
+        />
+      ))}
     </div>
+        </div>
+      </div>
   <h1>Velouré</h1>
 <p>Premiun Streetwear</p>
   </div>
