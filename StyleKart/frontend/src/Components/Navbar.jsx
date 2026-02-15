a3ce23a1f5ab2368bd55e9dd60ba982c517d6376
Kunal Kumar <kakeshiash@gmail.com>
2026-02-15 13:32:00 +0530

    push

--- StyleKart/frontend/src/Components/Navbar.jsx
+++ StyleKart/frontend/src/Components/Navbar.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
 import './Navbar.css'
+
 export default function Navbar(){
- return(<nav>
+
+ const [open,setOpen] = useState(false);
+
+ return(
+ <nav>
+
     <h2>Velouré</h2>
-    <div>
+
+    
+    <div className={`links ${open ? "show" : ""}`}>
         <a href="#">Men</a>
         <a href="#">Women</a>
         <a href="#">Kids</a>
     </div>
-    <div>
+
+   
+    <div className="actions">
         <button>Cart</button>
         <button>Login</button>
     </div>
+
+   
+    <div className={`hamburger ${open ? "active":""}`}
+         onClick={()=>setOpen(!open)}>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+
 </nav>
  );
 }
